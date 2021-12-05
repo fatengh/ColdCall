2ed7a9126f57b6aa7c6beebb2cd9996ae88d3599
fatengh <faten.arg@hotmail.com>
2021-12-05 19:18:49 +0000

    first commit

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLebel: UILabel!
+    //array of names
+    let names  = ["Faten","Manar","Fadia","Rawya","Ruba"]
+
+    @IBAction func coldCallButtonPress(_ sender: UIButton) {
+        //change lebel by random index of array
+        nameLebel.text = names[Int.random(in: 0..<5)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
